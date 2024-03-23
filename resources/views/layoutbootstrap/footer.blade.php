6af04f843ad938bad56d25ddb1b7d0383147a46b
kaniachyntia <kaniachyntiaa@gmail.com>
2024-03-23 20:47:21 +0700

    fix

--- resources/views/layoutbootstrap/footer.blade.php
+++ resources/views/layoutbootstrap/footer.blade.php
@@ -22,7 +22,7 @@
   <script src="{{asset('vendor/datatables/dataTables.bootstrap4.min.js')}}"></script>
 
     <!-- Page level custom scripts -->
-    <script src="{{asset('js/demo/datatables-demo.js')}}"></script>      
+    <script src="{{asset('js/demo/datatables-demo.js')}}"></script>    
 
 </body>
 
